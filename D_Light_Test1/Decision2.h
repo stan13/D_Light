04e2afe1cbd96f56bf42a64f94ac97bec8588879
c-christiansen-87 <c_christiansen_87@yahoo.com.au>
2015-09-18 17:38:20 +0800

    decision/answer (bug in next button in answer 1 slide to be fixed later)

---
+++ D_Light_Test1/Decision2.h
@@ -0,0 +1,18 @@
+//
+//  Decision2.h
+//  D_Light_Test1
+//
+//  Created by Candice Christiansen on 18/09/2015.
+//  Copyright (c) 2015 Nathan Stanley. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Foundation/Foundation.h>
+
+@interface Decision2 : ViewController
+
+//Public Variables
+- (IBAction)changeMinutes:(UISlider *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *minutesLabel;
+
+@end
