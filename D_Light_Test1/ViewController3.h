f803850bde44b15bd0daf83c4cb548405a5a06a0
stan13 <nathanastanley@gmail.com>
2015-09-28 05:46:04 +0800

    template for clothes customisation improved  - still need to fill in methods for buttons and complete viewDidLoad  - slider for colours not yet implemented

--- D_Light_Test1/ViewController3.h
+++ D_Light_Test1/ViewController3.h
@@ -11,8 +11,38 @@
 #import <Foundation/Foundation.h>
 
 @interface ViewController3 : ViewController
+//Image Views
 
-//Public Variables
+@property (weak, nonatomic) IBOutlet UIImageView *baseView;
+@property (weak, nonatomic) IBOutlet UIImageView *topView;
+@property (weak, nonatomic) IBOutlet UIImageView *bottomView;
+@property (weak, nonatomic) IBOutlet UIImageView *shoesView;
+@property (weak, nonatomic) IBOutlet UIImageView *glassesView;
+@property (weak, nonatomic) IBOutlet UIImageView *hatView;
+//tops
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *top;
+ - (IBAction)Singlet:(UIButton *)sender;
+ - (IBAction)Shirt0:(UIButton *)sender;
+ - (IBAction)Shirt1:(UIButton *)sender;
+//bottoms
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *bottom;
+ - (IBAction)Shorts0:(UIButton *)sender;
+ - (IBAction)Shorts1:(UIButton *)sender;
+ - (IBAction)Pants:(UIButton *)sender;
+//shoes
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *shoes;
+ - (IBAction)Shoes0:(UIButton *)sender;
+ - (IBAction)Shoes1:(UIButton *)sender;
+ - (IBAction)Shoes2:(UIButton *)sender;
+//excessories
+ - (IBAction)glasses:(UIButton *)sender;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *hat;
+ - (IBAction)hat0:(UIButton *)sender;
+ - (IBAction)hat1:(UIButton *)sender;
+
+
+
+
 
 @end
 
--- D_Light_Test1/ViewController3.m
+++ D_Light_Test1/ViewController3.m
@@ -10,7 +10,20 @@
 
 @interface ViewController3 ()
 //Private Variables
-@property (weak, nonatomic) IBOutlet UILabel *boyOrGirl;
+//cust variables
+@property int currentTop;
+@property CGFloat currentTopHue;
+@property int currentBottom;
+@property CGFloat currentBottomHue;
+@property int currentShoes;
+@property CGFloat currentShoeHue;
+@property int currentglasses;
+@property CGFloat currentGlassesHue;
+@property int currentHat;
+@property CGFloat currentHatHue;
+//loaded from defaults
+@property NSInteger sex;
+
 
 @end
 
@@ -22,27 +35,83 @@
 {
     [super viewDidLoad];
     //put code here or call another method
-    [self showSex];
+    [self setUp];
 }
-- (void) showSex
-{
-    //self.boyOrGirl.textColor = [UIColor redColor];
-    
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSInteger sex = [defaults integerForKey:@"sex"];
-    CGFloat r = [defaults floatForKey:@"skinRed"];
-    CGFloat g = [defaults floatForKey:@"skinGreen"];
-    CGFloat b = [defaults floatForKey:@"skinBlue"];
-    if (sex == 0) {
-        self.boyOrGirl.text = [NSString stringWithFormat:@"BOY %f %f %f", r, g, b];
-        
-    }
-    else if (sex == 1) {
-        self.boyOrGirl.text = [NSString stringWithFormat:@"BOY %f %f %f", r, g, b];
 
+- (void) setUp {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    self.sex = [defaults integerForKey:@"sex"];
+    CGFloat eyeHue = [defaults floatForKey:@"eyeHue"];
+    CGFloat skinRed = [defaults floatForKey:@"skinRed"];
+    CGFloat skinGreen = [defaults floatForKey:@"skinGreen"];
+    CGFloat skinBlue = [defaults floatForKey:@"skinBlue"];
+    CGFloat hairHue = [defaults floatForKey:@"hairHue"];
+    //set buttons to images based on gender
+    NSString *sexString;
+    if (self.sex == 1) {
+        sexString = @"girl";
+    } else {
+        sexString = @"boy";
     }
+    //sets up image for base
+    UIImage *temp = [UIImage imageNamed:[NSString stringWithFormat:@"%@Face",sexString]];
+    //applies image with skin tone from presets
+    self.baseView.image = [self changeImage:temp toColour: [UIColor colorWithRed:skinRed green:skinGreen blue:skinBlue alpha:1]];
     
+}
+
+
+//BUTTONS
+//Change tops
+- (IBAction)Singlet:(UIButton *)sender {
+}
+- (IBAction)Shirt0:(UIButton *)sender {
+}
+- (IBAction)Shirt1:(UIButton *)sender {
+}
+//Change Bottoms
+- (IBAction)Shorts0:(UIButton *)sender {
+}
+- (IBAction)Shorts1:(UIButton *)sender {
+}
+- (IBAction)Pants:(UIButton *)sender {
+}
+//Change Shoes
+- (IBAction)Shoes0:(UIButton *)sender {
+}
+- (IBAction)Shoes1:(UIButton *)sender {
+}
+- (IBAction)Shoes2:(UIButton *)sender {
+}
+//select glasses
+- (IBAction)glasses:(UIButton *)sender {
+}
+//select hat
+- (IBAction)hat0:(UIButton *)sender {
+}
+- (IBAction)hat1:(UIButton *)sender {
+}
+
+
+//changes the colour of images - for eyes, hair, clothes etc
+- (UIImage *) changeImage: (UIImage *)img toColour: (UIColor *)colour
+{
+    //code modified from https://coffeeshopped.com/2010/09/iphone-how-to-dynamically-color-a-uiimage
+    
+    UIGraphicsBeginImageContext(img.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [colour setFill];
+    CGContextTranslateCTM(context, 0, img.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    CGContextSetBlendMode(context, kCGBlendModeOverlay);
+    CGRect rect = CGRectMake(0, 0, img.size.width, img.size.height);
+    CGContextDrawImage(context, rect, img.CGImage);
+    CGContextClipToMask(context, rect, img.CGImage);
+    CGContextAddRect(context, rect);
+    CGContextDrawPath(context,kCGPathFill);
+    UIImage *newImg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImg;
 }
 
 
