b58805d8d6057e86a747717bf4b97429ce24fd26
c-christiansen-87 <c_christiansen_87@yahoo.com.au>
2015-09-29 15:14:18 +0800

    bug fix

--- D_Light_Test1/Decison8.m
+++ D_Light_Test1/Decison8.m
@@ -6,4 +6,37 @@
 //  Copyright (c) 2015 Nathan Stanley. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "Decision8.h"
+
+@interface Decision8 ()
+//Private Variables
+
+
+@end
+
+@implementation Decision8
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    //put code here or call another method
+    self.nextButton.enabled = NO;
+}
+
+
+- (IBAction)chooseMore:(UIButton *)sender {
+    self.nextButton.enabled = YES;
+    self.moreButton.backgroundColor = [UIColor grayColor];
+    self.lessButton.backgroundColor = [UIColor clearColor];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:1 forKey:@"equator"];
+}
+- (IBAction)chooseLess:(UIButton *)sender {
+    self.nextButton.enabled = YES;
+    self.moreButton.backgroundColor = [UIColor clearColor];
+    self.lessButton.backgroundColor = [UIColor grayColor];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:2 forKey:@"equator"];
+}
+@end
+
